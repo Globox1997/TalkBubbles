b0b2c60f7bec685e01f332c35fc388390ae0fa4a
Globox1997 <mitschke11@web.de>
2022-08-22 09:55:47 +0200

    updated to 1.19.2

--- src/main/java/net/talkbubbles/mixin/ChatHudMixin.java
+++ src/main/java/net/talkbubbles/mixin/ChatHudMixin.java
@@ -2,42 +2,46 @@ package net.talkbubbles.mixin;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.At;
 
-import net.fabricmc.api.Environment;
-import net.fabricmc.api.EnvType;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.ChatHudListener;
+import net.minecraft.client.font.TextVisitFactory;
+import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.gui.hud.MessageIndicator;
 import net.minecraft.client.network.OtherClientPlayerEntity;
-import net.minecraft.network.message.MessageSender;
-import net.minecraft.network.message.MessageType;
+import net.minecraft.network.message.MessageSignatureData;
 import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.text.Text;
+import net.minecraft.util.Util;
 import net.talkbubbles.TalkBubbles;
 import net.talkbubbles.accessor.OtherClientPlayerEntityAccessor;
 
-@Environment(EnvType.CLIENT)
-@Mixin(ChatHudListener.class)
-public class ChatHudListenerMixin {
+@Mixin(ChatHud.class)
+public class ChatHudMixin {
+
     @Shadow
     @Final
+    @Mutable
     private MinecraftClient client;
 
-    @Inject(method = "onChatMessage", at = @At("HEAD"))
-    private void onChatMessageMixin(MessageType type, Text message, @Nullable MessageSender sender, CallbackInfo info) {
-        if (sender != null && type.chat().isPresent()) {
+    // onChatMessage is now done in MessageHandler.class
+    @Inject(method = "Lnet/minecraft/client/gui/hud/ChatHud;addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;Lnet/minecraft/client/gui/hud/MessageIndicator;)V", at = @At("HEAD"))
+    private void addMessageMixin(Text message, @Nullable MessageSignatureData signature, @Nullable MessageIndicator indicator, CallbackInfo info) {
+        if (extractSender(message) != null) {
             List<OtherClientPlayerEntity> list = client.world.getEntitiesByClass(OtherClientPlayerEntity.class, client.player.getBoundingBox().expand(TalkBubbles.CONFIG.chatRange),
                     EntityPredicates.EXCEPT_SPECTATOR);
             for (int i = 0; i < list.size(); i++)
-                if (list.get(i).getUuid().equals(sender.uuid())) {
+                if (list.get(i).getUuid().equals(extractSender(message))) {
                     String stringMessage = message.getString();
                     stringMessage = stringMessage.replace("<" + StringUtils.substringBetween(stringMessage, "<", ">") + "> ", "");
                     String[] string = stringMessage.split(" ");
@@ -81,5 +85,15 @@ public class ChatHudListenerMixin {
                     break;
                 }
         }
+
+    }
+
+    private UUID extractSender(Text text) {
+        String string = TextVisitFactory.removeFormattingCodes(text);
+        String string2 = StringUtils.substringBetween(string, "<", ">");
+        if (string2 == null) {
+            return Util.NIL_UUID;
+        }
+        return this.client.getSocialInteractionsManager().getUuid(string2);
     }
 }

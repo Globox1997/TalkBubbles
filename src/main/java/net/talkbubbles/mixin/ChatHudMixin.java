1cc8ed5b8e6cb77fd582de2b1bf03c5c72cf390d
Belcar Santiago Cuentas-Zavala Infante <mantrap.tartan0c@icloud.com>
2023-06-29 18:59:18 -0500

    Improve the removal of player names from bubble

--- src/main/java/net/talkbubbles/mixin/ChatHudMixin.java
+++ src/main/java/net/talkbubbles/mixin/ChatHudMixin.java
@@ -52,7 +52,8 @@ public class ChatHudMixin {
             for (int i = 0; i < list.size(); i++)
                 if (list.get(i).getUuid().equals(senderUUID)) {
                     String stringMessage = message.getString();
-                    stringMessage = stringMessage.replaceFirst("(§.)?\\W+" + detectedSenderName + "[^\\w§]+(§.)?", "");
+                    stringMessage = stringMessage.replaceFirst("[\\s\\S]+" + detectedSenderName + "([^\\w§]|(§.)?)+\\s+", "");
+                    TalkBubbles.LOGGER.error(stringMessage);
                     String[] string = stringMessage.split(" ");
                     List<String> stringList = new ArrayList<>();
                     String stringCollector = "";
